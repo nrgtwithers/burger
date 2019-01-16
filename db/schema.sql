feb3fee6a9cd74efcf5764323dd2d0a29a5fb2d4
Nina Withers <ninarosewithers@gmail.com>
2019-01-15 22:14:20 -0500

    adding content to js files

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+  id INT AUTO_INCREMENT,
+  burger_name VARCHAR(30) NOT NULL,
+  devoured BOOLEAN
+  PRIMARY KEY(id)
+);
+
+SELECT * FROM burgers;
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+ INSERT INTO burgers
+    (burger_name, devoured)
+  VALUES
+    ("Hamburger", false);
+  INSERT INTO schools
+    (burger_name, devoured)
+  VALUES
+    ("Cheeseburger", true);
+  INSERT INTO schools
+    (burger_name, devoured)
+  VALUES
+    ("Veggie Burger", true);
