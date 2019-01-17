9d00b202fac7107c667b73e0f95abcb25a918a75
Nina Withers <ninarosewithers@gmail.com>
2019-01-17 14:40:18 -0500

    changes to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,39 @@
 var mysql = require("mysql");
+var connection;
 
-// create the connection information for the sql database
-var connection = mysql.createConnection({
-  host: "localhost",
+if(process.env.JAWSDB_URL){
+  connection =mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: '',
+    database: "burgers_db"
+  });
+}
 
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "",
-  database: "burgers_db"
-});
-
-// connect to the mysql server and sql database
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-});
+connection.connect();
 
 module.exports = connection;
+// create the connection information for the sql database
+// var connection = mysql.createConnection({
+//   host: "localhost",
+
+//   // Your port; if not 3306
+//   port: 3306,
+
+//   // Your username
+//   user: "root",
+
+//   // Your password
+//   password: "",
+//   database: "burgers_db"
+// });
+
+// connect to the mysql server and sql database
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("connected as id " + connection.threadId + "\n");
+// });
+
+// module.exports = connection;
