4eec0c88bf524655d3711abe970ee178f6dda868
Nina Withers <ninarosewithers@gmail.com>
2019-01-16 23:17:51 -0500

    changes to orm.js and index.hb

--- config/orm.js
+++ config/orm.js
@@ -2,16 +2,22 @@ var connection = require("../config/connection.js");
 
 var orm = {
     selectAll: function (callback) {
-    connection.query("SELECT * FROM burgers", function(err, res){
-        if (err) throw err;
-        callback(res);
-    })
+        connection.query("SELECT * FROM burgers", function (err, res) {
+            if (err) throw err;
+            callback(res);
+        })
     },
-    insertOne: function () {
-    
+    insertOne: function (callback) {
+        connection.query("INSERT FROM burgers SET ?", function (err, res) {
+            if (err) throw err;
+            callback(res);
+        })
     },
-    updateOne: function() {
-
+    updateOne: function () {
+        connection.query("'UPDATE burgers SET ? WHERE ?'", function (err, res) {
+            if (err) throw err;
+            callback(res);
+        })
     },
     name: "nina"
 }
